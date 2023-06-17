b6bc141df61da26218c52eb83b9b0af3f2720ad2
gordominossi <gordominossi@gmail.com>
2023-06-17 14:58:19 -0300

    test(language-extensions): add test case

--- lib/language-extensions._spec.lua
+++ lib/language-extensions._spec.lua
@@ -11,7 +11,7 @@ describe('language extensions', function()
             three = 3,
         }
 
-        it('Should create a table with the same values', function()
+        it('Should clone a table', function()
             local result = mergeTables(fakeTable)
 
             assert.same(fakeTable, result)
@@ -21,12 +21,33 @@ describe('language extensions', function()
         it(
             'Should override the values of the first table with the second one',
             function()
-                local result = mergeTables(fakeTable, { 'four', three = 14 })
+                local result = mergeTables(fakeTable, { 'newOne', three = 14 })
 
                 assert.not_same(fakeTable, result)
-                assert.equal('four', result[1])
+                assert.not_equal(fakeTable, result)
+
+                assert.equal(fakeTable[2], result[2])
+                assert.equal('newOne', result[1])
                 assert.equal(14, result.three)
             end
         )
+
+        it('Should merge many tables', function()
+            local result = mergeTables(
+                fakeTable,
+                { 'newOne', three = 14 },
+                { [2] = 'newTwo', five = 5 },
+                { [4] = 'four' }
+            )
+
+            assert.not_same(fakeTable, result)
+            assert.not_equal(fakeTable, result)
+
+            assert.equal('newTwo', result[2])
+            assert.equal('newOne', result[1])
+            assert.equal(14, result.three)
+            assert.equal(5, result.five)
+            assert.equal('four', result[4])
+        end)
     end)
 end)
