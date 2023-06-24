dd05ff703758c7de89cef51174a698a045a5a942
gordominossi <gordominossi@gmail.com>
2023-06-24 19:21:37 -0300

    test(layout): add default style and improve readability

--- src/gui/engine/layout/layout._spec.lua
+++ src/gui/engine/layout/layout._spec.lua
@@ -12,27 +12,6 @@ describe('Layout engine', function()
     local layout = Layout.new()
     local parser = Parser.new()
 
-    ---@type Component
-    local fakeComponent = {
-        style = { width = 20 },
-        { 'Fake text', style = { color = colors.primary } },
-    }
-
-    local fakeElement = parser.execute(fakeComponent)
-
-    ---@type LayoutObject
-    local fakeBlockLayout = {
-        node = fakeElement,
-        parent = {},
-        previous = {},
-        children = {},
-        width = 20,
-        height = 1,
-        x = 0,
-        y = 0,
-        style = { width = 20, display = 'block', color = colors.background },
-    }
-
     describe('text', function()
         local fakeText = parser.execute({
             'Fake text',
@@ -60,37 +39,63 @@ describe('Layout engine', function()
 
             assert.same(fakeTextLayout, result)
         end)
+
+        it('should have a default style', function()
+            local input = parser.execute({ 'text' })
+
+            local result = layout.execute(input)
+
+            local expectedStyle = { display = 'inline', color = colors.white }
+
+            assert.same(expectedStyle, result.style)
+        end)
     end)
 
-    --[[
-    --  it('should nest children', function()
-    --     local result = layout.execute(fakeElement)
-
-    --     assert.same(
-    --         { mergeTables(
-    --             fakeTextLayout,
-    --             { parent = result, width = result.width }
-    --         ) },
-    --         result.children
-    --     )
-    -- end)
-    --]]
-
     describe('block', function()
+        ---@type Component
+        local fakeComponent = {
+            style = { width = 20 },
+            { 'Fake text', style = { color = colors.primary } },
+        }
+
+        local fakeElement = parser.execute(fakeComponent)
+
+        ---@type LayoutObject
+        local fakeBlockLayout = {
+            node = fakeElement,
+            parent = {},
+            previous = {},
+            children = {},
+            width = 20,
+            height = 1,
+            x = 0,
+            y = 0,
+            style = {
+                color = colors.background,
+                display = 'block',
+                width = 20,
+            },
+        }
+
+        it('should have a default style', function()
+            local input = parser.execute({ type = 'div' })
+
+            local result = layout.execute(input)
+
+            local expectedStyle = {
+                display = 'block',
+                color = colors.background,
+            }
+
+            assert.same(expectedStyle, result.style)
+        end)
+
         it('should layout a node with a text node child', function()
             local result = layout.execute(fakeElement)
 
+            local child = mergeTables(result.children[1], { parent = result })
             assert.same(
-                mergeTables(
-                    fakeBlockLayout,
-                    {
-                        children = {
-                            mergeTables(result.children[1],
-                                { parent = result })
-
-                        }
-                    }
-                ),
+                mergeTables(fakeBlockLayout, { children = { child } }),
                 result
             )
         end)
