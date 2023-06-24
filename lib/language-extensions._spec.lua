0c97880ff12f49dffdfeecbeeef82cbe888df738
gordominossi <gordominossi@gmail.com>
2023-06-24 13:57:47 -0300

    test(language-extensions): improve readability

--- lib/language-extensions._spec.lua
+++ lib/language-extensions._spec.lua
@@ -1,7 +1,7 @@
-local describe = _ENV.describe
-local it = _ENV.it
-
 local mergeTables = require('lib.language-extensions').mergeTables
+
+local describe = os.getenv('describe') or _ENV.describe
+local it = os.getenv('it') or _ENV.it
 
 describe('language extensions', function()
     describe('mergeTables', function()
@@ -26,16 +26,21 @@ describe('language extensions', function()
                 assert.not_same(fakeTable, result)
                 assert.not_equal(fakeTable, result)
 
-                assert.equal(fakeTable[2], result[2])
-                assert.equal('newOne', result[1])
-                assert.equal(14, result.three)
+                assert.same(
+                    {
+                        'newOne',
+                        fakeTable[2],
+                        three = 14
+                    },
+                    result
+                )
             end
         )
 
         it('Should merge many tables', function()
             local result = mergeTables(
                 fakeTable,
-                { 'newOne', three = 14 },
+                { 'newOne', 'overridenTwo', three = 14 },
                 { [2] = 'newTwo', five = 5 },
                 { [4] = 'four' }
             )
@@ -43,11 +48,17 @@ describe('language extensions', function()
             assert.not_same(fakeTable, result)
             assert.not_equal(fakeTable, result)
 
-            assert.equal('newTwo', result[2])
-            assert.equal('newOne', result[1])
-            assert.equal(14, result.three)
-            assert.equal(5, result.five)
-            assert.equal('four', result[4])
+            assert.same(
+                {
+                    'newOne',
+                    'newTwo',
+                    three = 14,
+                    nil,
+                    'four',
+                    five = 5,
+                },
+                result
+            )
         end)
     end)
 end)
