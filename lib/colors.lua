25dfd75b5a28c66c174a62363e362283ea6b72c2
gordominossi <gordominossi@gmail.com>
2023-06-24 19:01:08 -0300

    feat(colors): add constants

---
+++ lib/colors.lua
@@ -0,0 +1,14 @@
+local colors = {
+    background = 0x000000,
+    border = 0x2D2D2D,
+    disabled = 0x666666,
+    error = 0xFF6666,
+    info = 0x66FFFF,
+    primary = 0xFF9966,
+    secondary = 0x9966FF,
+    success = 0x66FF99,
+    warning = 0xCCFF66,
+    white = 0xFFFFFF,
+}
+
+return colors
