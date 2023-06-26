783e03a2a3b41d6fe5beee04170772b36212d90f
RafaelTestlio <133226456+RafaelTestlio@users.noreply.github.com>
2023-06-26 13:37:59 -0300

    feat(paint): build a paint array

---
+++ src/gui/engine/paint/init.lua
@@ -0,0 +1,37 @@
+---@type Paint
+local Paint = {}
+
+---@return Paint
+function Paint.new()
+    ---@class Paint
+    local self = {}
+
+    ---@param input LayoutObject
+    ---@return PaintObject[]
+    function self.execute(input)
+        ---@type PaintObject[] list
+        local result = {}
+
+        if (input.node.type == 'text') then
+            table.insert(result, {
+                x = input.x,
+                y = input.y,
+                value = input.node.value,
+                vertical = false,
+            })
+        end
+
+        for _, child in ipairs(input.children) do
+            local childList = self.execute(child)
+            for _, paintObject in ipairs(childList) do
+                table.insert(result,paintObject)
+            end
+        end
+
+        return result
+    end
+
+    return self
+end
+
+return Paint
---
+++ src/gui/engine/paint/paint._spec.lua
@@ -0,0 +1,91 @@
+local describe = _ENV.describe
+local it = _ENV.it
+
+local colors = require('lib.colors')
+
+local Layout = require('src.gui.engine.layout')
+local Parser = require('src.gui.engine.parser.parser')
+local Paint = require('src.gui.engine.paint')
+
+---@class PaintObject
+---@field x integer
+---@field y integer
+---@field value string
+---@field vertical boolean
+
+describe('paint', function()
+    local layout = Layout.new()
+    local parser = Parser.new()
+    local paint = Paint.new()
+
+    it('Should create a list that can be send to the gpu api', function()
+        local parsedComponent = parser.execute({'text'})
+        local input = layout.execute(parsedComponent)
+
+        local result = paint.execute(input)
+
+        local expectedList = {
+            {
+                x = 0,
+                y = 0,
+                value = 'text',
+                vertical = false,
+            }
+        }
+        assert.same(expectedList, result)
+    end)
+
+    it('Should create a list that can be send to the gpu api2', function()
+        local parsedComponent = parser.execute({
+            {'text'},
+            {'text'},
+        })
+        local input = layout.execute(parsedComponent)
+
+        local result = paint.execute(input)
+
+        local expectedList = {
+            {
+                x = 0,
+                y = 0,
+                value = 'text',
+                vertical = false,
+            },
+            {
+                x = 0,
+                y = 0,
+                value = 'text',
+                vertical = false,
+            },
+        }
+        assert.same(expectedList, result)
+    end)
+
+    it('Should create a list from nested components', function ()
+        local parsedComponent = parser.execute({{
+            {'text'},
+            {'text'},
+        }})
+        local input = layout.execute(parsedComponent)
+
+        local result = paint.execute(input)
+
+        local expectedList = {
+            {
+                x = 0,
+                y = 0,
+                value = 'text',
+                vertical = false,
+            },
+            {
+                x = 0,
+                y = 0,
+                value = 'text',
+                vertical = false,
+            },
+        }
+        assert.same(expectedList, result)
+    end)
+
+
+end)
